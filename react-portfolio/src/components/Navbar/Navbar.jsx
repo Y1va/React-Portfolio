7d3f069ded661f84ed29daac0430698e2200f9d0
ZeuS <ponzeadrian@gmail.com>
2024-04-23 18:17:11 +1000

    added media queries for navbar css & functionality to open/close hamburger icon

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import styles from './Navbar.module.css';
 import { getImageUrl } from '../../utils';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
@@ -12,8 +14,13 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={getImageUrl('nav/menuIcon.png')}
+          src={
+            menuOpen
+              ? getImageUrl('nav/closeIcon.png')
+              : getImageUrl('nav/menuIcon.png')
+          }
           alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
         />
         <ul className={styles.menuItems}>
           <li>
